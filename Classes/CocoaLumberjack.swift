b2deb41e4b3feeecc57d2f78d3bb2098a6c07195
Matthew Crenshaw <mcrenshaw@zipari.com>
2018-05-08 14:01:34 -0400

    casting UInt to Int

--- Classes/CocoaLumberjack.swift
+++ Classes/CocoaLumberjack.swift
@@ -55,7 +55,7 @@ public func resetDefaultDebugLevel() {
 public func _DDLogMessage(_ message: @autoclosure () -> String, level: DDLogLevel, flag: DDLogFlag, context: Int, file: StaticString, function: StaticString, line: UInt, tag: Any?, asynchronous: Bool, ddlog: DDLog) {
     if level.rawValue & flag.rawValue != 0 {
         // Tell the DDLogMessage constructor to copy the C strings that get passed to it.
-        let logMessage = DDLogMessage(message: message(), level: level, flag: flag, context: context, file: String(describing: file), function: String(describing: function), line: line, tag: tag, options: [.copyFile, .copyFunction], timestamp: nil)
+        let logMessage = DDLogMessage(message: message(), level: level, flag: flag, context: context, file: String(describing: file), function: String(describing: function), line: Int(line), tag: tag, options: [.copyFile, .copyFunction], timestamp: nil)
         ddlog.log(asynchronous: asynchronous, message: logMessage)
     }
 }
